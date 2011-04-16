3f36d0e255f176562495dd5330c71213aa73111c
松田 洋祐 <matsudayousuke@matsudayousuke.local>
2011-04-16 15:55:38 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lift::Application.config.secret_token = '429cf1b5dac512c92a24ef6584c6c70dfe6cfd232cce2f2383da49af93ae0328be8a73daaec0fa6ad7f425ea3e1407b8bf5115729b62c9dc48f223b099bf8b9e'
