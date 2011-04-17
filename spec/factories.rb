57ade70a342a68be827ee30be21dc46b9a254445
matsudayousuke <matsudayousuke@gmail.com>
2011-04-17 12:53:55 +0900

    FactoryGirlでテストを実装

---
+++ spec/factories.rb
@@ -0,0 +1,4 @@
+Factory.define :objective do |objective|
+  objective.name          "master TDD"
+  objective.description   "master TDD on Rails app"
+end
--- spec/models/objective_spec.rb
+++ spec/models/objective_spec.rb
@@ -3,29 +3,29 @@
   describe Objective do
 
     before(:each) do
-      @attr = { :name => "目標１", :description => "まずをこれを会得する" }
+      @objective = Factory(:objective)
     end
 
 	describe "必須項目チェック" do
 		it "名前が未入力の場合" do
-		  objective = Objective.new(@attr.merge(:name => ""))
-		  objective.should_not be_valid
+		  @objective.name = ""
+		  @objective.should_not be_valid
 		end
 
 		it "上記以外の場合(＝正常パターン)" do
-		  Objective.create!(@attr)
+		  @objective.should be_valid
 		end
     end
 
 	describe "桁数チェック" do
 		it "名前がmaxlength+1の場合" do
-		  objective = Objective.new(@attr.merge(:name => "a" * 101))
-		  objective.should_not be_valid
+		  @objective.name = "a" * 101
+		  @objective.should_not be_valid
 		end
 
 		it "上記以外の場合(＝正常パターン)" do
-		  objective = Objective.new(@attr.merge(:name => "a" * 100))
-		  objective.should be_valid
+		  @objective.name = "a" * 100
+		  @objective.should be_valid
 		end
     end
   end
